832eb1a69d1e95cf38b2351ebf87d05b05e3e0f5
DavidTangJK <davidtangjk@gmail.com>
2022-10-20 14:51:08 -0500

    Changed background color, added title

--- game.js
+++ game.js
@@ -36,7 +36,7 @@ function nextSequence() {
   $("#" + randomChosenColor).fadeOut(300).fadeIn(300);
   playSound(randomChosenColor);
   level++;
-  $("h1").text("Level " + level);
+  $("#level-title").text("Level " + level);
 }
 
 function playSound(name) {
@@ -65,7 +65,7 @@ function checkAnswer(currentLevel) {
     setTimeout(function() {
       $("body").removeClass("game-over");
     }, 200);
-    $("h1").text("Game Over, Press Any Key to Restart");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
     startOver();
   }
 }
